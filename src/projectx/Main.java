30636acc05ad4607e2f555dd5a9aaf49b1b580c8
Stefan <Stefan@Stefan-PC>
2014-11-28 20:34:01 +0100

    Added some content

--- src/projectx/Main.java
+++ src/projectx/Main.java
@@ -1,10 +1,29 @@
 package projectx;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Dingen");
+		
+		//init the array to store ways to get richs
+		ArrayList<String> geldVerdienManieren = new ArrayList<String>();
+		
+		//Add ways to get richs
+		geldVerdienManieren.add("Een rock band beginnen");
+		geldVerdienManieren.add("Alleen maar zealots bouwen en daarna max tempest gaan..");
+		
+		//Output the explenation for the UI
+		System.out.println("Hoe rijk te worden in 2 stappen:");
+		
+		
+		//Output each way through an aweomse for-loop
+		for (int i = 0; i < geldVerdienManieren.size(); i++) {
+			System.out.println(geldVerdienManieren.get(i));
+		}
+		
+		//gg, wp sir
+		
 	}
 
 }
