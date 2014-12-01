6916a455a194123ae8c05d3b3a5e340a84f1986f
FrostUpload <stefan@budgetslots.com>
2014-12-01 23:10:33 +0100

    Added package which contains the fisrt classes of the commandcenter simulation simulator

---
+++ src/main/java/projectx/commandcenter/Commandcenter.java
@@ -0,0 +1,33 @@
+package main.java.projectx.commandcenter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Commandcenter {
+	
+	private List<Unit> units;
+	
+	public static void main(String[] args) {
+		Commandcenter cc = new Commandcenter();
+		cc.buildUnits();
+		cc.showUnits();
+	}
+	
+	private void showUnits () {
+		units.stream()
+		.filter(u->u.getHealth() > 50)
+		.map(u->u.getName())
+		.forEach(System.out::println);
+	}
+	
+	private void buildUnits() {
+		Unit marine = new Unit(45, "Marine", false);
+		Unit marauder = new Unit(120, "Marauder", true);
+		Unit reaper = new Unit(60, "Reaper", true);
+		List<Unit> units = new ArrayList<Unit>();
+		units.addAll(Arrays.asList(marine,marauder,reaper));
+		this.units = units;
+	}
+}
---
+++ src/main/java/projectx/commandcenter/Unit.java
@@ -0,0 +1,26 @@
+package main.java.projectx.commandcenter;
+
+
+public class Unit {
+	private int hitPoints;
+	private String name;
+	private boolean armored;
+	
+	public Unit(int hitPoints, String name, boolean armor) {
+		this.hitPoints = hitPoints;
+		this.name = name;
+		this.armored = armor;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getHealth() {
+		return this.hitPoints;
+	}
+	
+	public boolean getArmor() {
+		return this.armored;
+	}
+}
